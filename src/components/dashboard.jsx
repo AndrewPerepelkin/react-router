499744908deccac6fced87082568f0cdaabfe5d9
AndrewPerepelkin <avp.tep@rambler.ru>
2022-10-12 02:28:28 +0300

    feat: dashboard routing

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import Edit from './edit';
+import Stats from './stats';
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+    <ul>
+      <li>
+        <Link to='/dashboard/edit'>Edit</Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>Dashboard</Link>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path='/dashboard/' component={Stats}/>
+      <Route path='/dashboard/edit' component={Edit}/>
+    </Switch>
+    </div>
+    
   );
 };
 
